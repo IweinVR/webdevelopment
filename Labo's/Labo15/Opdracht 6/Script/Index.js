4f49679c11b3c44993df7568df3dc1cd515fe0c2
IweinVR <r0999297@student.vives.be>
2025-02-14 11:11:19 +0100

    Update

--- Labo's/Labo15/Opdracht 6/Script/Index.js
+++ Labo's/Labo15/Opdracht 6/Script/Index.js
@@ -2,11 +2,12 @@ const setup = () => {
     let btnKopieer = document.getElementById("btnKopieer");
     btnKopieer.addEventListener("click", kopieer);
 }
+
 const kopieer = () => {
     let txtInput = document.getElementById("txtInput");
     let tekst = txtInput.value;
-    document.getElementById("txtOutput").innerHTML = tekst;
-
-
+    let txtOutput = document.getElementById("txtOutput");
+    txtOutput.textContent = tekst;
 }
+
 window.addEventListener("load", setup);
